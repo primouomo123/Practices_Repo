58561f4e1909238394fe3494a3c44a1902615f4e
primouomo123 <luismanuelrios123@gmail.com>
2025-06-11 09:11:49 -0400

    Corrections in a comment

--- JavaScript_Practices/multiplication_table.js
+++ JavaScript_Practices/multiplication_table.js
@@ -1,7 +1,7 @@
 //Control variable
 multiplicand = 12, multiplier = 12;
 
-//For loop to creat multiplication tables
+//For loop to create multiplication tables
 for (a = 1; a <= multiplicand; a++) {
     console.log(`Multiplication Table of ${a}:`);
     for (b = 1; b <= multiplier; b++) {
